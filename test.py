c2b773d7da720bff78c335904f213095b2628c32
Yugo Omura <yomura@YugonoMacBook-puro.local>
2020-05-17 16:51:20 +0900

    test

--- test.py
+++ test.py
@@ -6,6 +6,6 @@ p = argparse.ArgumentParser()
 p.add_argument("tale_java_output")
 args = p.parse_args()
 
-data = tale_java.load(args.tale_java_output)
+data = tajava.reader.tale.load(args.tale_java_output)
 for i_row, row in enumerate(data):
     print(f'{i_row}: {row["simu"]["logE0"] = :.3f}, {row["recon"]["logE0"] = :.3f}')
