fbbd250d2f4a0d89ed92b40b109bf3f511faaf5b
Yugo Omura <yomura@YugonoMacBook-puro.local>
2021-11-09 21:16:34 +0900

    modified:   tajava/reader/fd/simple2.py

--- tajava/reader/fd/simple2.py
+++ tajava/reader/fd/simple2.py
@@ -4,21 +4,29 @@ see TAFDReconstructor._printResultsSimple2
 
 import numpy as np
 from . import _util
-import re
-import itertools
 
 
 __all__ = ["load"]
 
-pattern = re.compile(r"""(\d+) (\d+) (\d+) (\d+) (\d+/\d+/\d+) (\d+:\d+:\d+\.\d+)
-""")
+
+def _parse_block(block: str):
+    lines = block.splitlines()
+    assert lines[1].startswith("Sim Geom ")
+    split_line1 = lines[1].split()
+    return (
+        _util.parse_event_info(*lines[0].split()),
+        (*split_line1[3:-3], tuple(split_line1[-3:]))
+    )
 
 
 def load(path):
     with open(path, "r") as f:
-        data = pattern.findall(f.read())
+        blocks = [stripped_block for block in f.read().split("\n\n") if (stripped_block := block.strip())]
 
-    event_info = np.array(
-        list(itertools.starmap(_util.parse_event_info, data)), dtype=_util.event_info_dtype_descr
+    return np.array(
+        list(map(_parse_block, blocks)),
+        dtype=[
+            ("event_info", _util.event_info_dtype_descr),
+            ("geom", [("zenith", "f4"), ("azimuth", "f4"), ("core_position", ("f4", 3))])
+         ]
     )
-    return event_info
